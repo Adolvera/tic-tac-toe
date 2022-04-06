4552604cf34c109ad7ac5a03d599a62e1404b722
Adolvera <aolvera4734@gmail.com>
2022-04-06 14:45:15 -0400

    Added array update when a move is made

--- main.js
+++ main.js
@@ -9,42 +9,47 @@ const gameBoard = (() => {
   const playerOneInput = document.querySelector('#player-one');
   const playerTwoInput = document.querySelector("#player-two");
   const formContainer = document.querySelector(".form-container");
+  const header = document.querySelector('#header');
+
+  boardDiv.addEventListener('click', (e) => displayController.makeChoice(e))
+  formButton.addEventListener('click', (e) => displayController.checkForm())
 
   return {
     gameArray,
-    boardDiv,
-    formButton,
     formContainer,
     playerOneInput,
-    playerTwoInput
+    playerTwoInput,
+    header
   }
 })();
 
 const displayController = (() => {
   let turn = 1;
 
-  const renderBoard = () => {
-    let counter = 1;
+  // const renderBoard = () => {
+  //   let counter = 1;
 
-    gameBoard.gameArray.forEach((square) => {
-      let squareContainer = document.querySelector(`#square-${counter.toString()}`);
-      let squareDiv = document.createElement('div');
-      squareDiv.classList.add('square');
-      squareDiv.textContent = `${square}`;
-      squareContainer.appendChild(squareDiv);
-      counter++;
-    })
-  }
+  //   gameBoard.gameArray.forEach((square) => {
+  //     let squareContainer = document.querySelector(`#square-${counter.toString()}`);
+  //     let squareDiv = document.createElement('div');
+  //     squareDiv.classList.add('square');
+  //     squareDiv.textContent = `${square}`;
+  //     squareContainer.appendChild(squareDiv);
+  //     counter++;
+  //   })
+  // }
   
   const makeChoice = (e) => {
     if (e.target.textContent != "") {
       return;
     }
     if (turn) {
-      e.target.textContent = jerry.sign;
+      e.target.textContent = "X";
+      runGame.updateArray(e.target.id, "X");
       turn = 0;
     } else {
-      e.target.textContent = ben.sign;
+      e.target.textContent = 'O';
+      runGame.updateArray(e.target.id, "O")
       turn = 1;
     }
   }
@@ -54,28 +59,87 @@ const displayController = (() => {
     const playerTwo = gameBoard.playerTwoInput.value;
 
     if (playerOne === "" || playerTwo === "") {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = "Please enter names for both fields!";
+      div.appendChild(p);
+      gameBoard.playerTwoInput.after(div);
       return
     } else {
       gameBoard.formContainer.classList.add('hidden');
-      startGame(playerOne, playerTwo);
+      runGame.playerAssign(playerOne, playerTwo);
     }
   }
 
-  const startGame = (one, two) => {
-    // const playerOne = Player(`${one}`, 'X');
-    // const playerTwo = Player(`${two}`, 'O');
+  const renderPlayers = (playerOne, playerTwo) => {
+    //Element creation for vs title card
+    const div = document.createElement("div");
+    const playerOneDiv = document.createElement("div");
+    const playerTwoDiv = document.createElement('div');
+    const pPlayerOne = document.createElement('p');
+    const pPlayerTwo = document.createElement('p');
+    const signPlayerOne = document.createElement('p');
+    const signPlayerTwo = document.createElement('p');
+    const versus = document.createElement('p');
+
+    //Text based on created players
+    pPlayerOne.textContent = `${playerOne.name}`;
+    pPlayerTwo.textContent = `${playerTwo.name}`;
+    signPlayerOne.textContent = `${playerOne.sign}`;
+    signPlayerTwo.textContent = `${playerTwo.sign}`;
+    versus.textContent = "VS.";
+
+    div.classList.add('flex');
+    playerOneDiv.classList.add("players");
+    playerTwoDiv.classList.add("players");
+    versus.classList.add("b-text");
+    signPlayerOne.classList.add("b-text");
+    signPlayerTwo.classList.add("b-text");
+
+    playerOneDiv.appendChild(pPlayerOne);
+    playerOneDiv.appendChild(signPlayerOne);
+    playerTwoDiv.appendChild(pPlayerTwo);
+    playerTwoDiv.appendChild(signPlayerTwo);
+
+    div.appendChild(playerOneDiv);
+    div.appendChild(versus);
+    div.appendChild(playerTwoDiv);
+    gameBoard.header.after(div);
   }
 
   return {
-    renderBoard,
     makeChoice,
-    checkForm
+    checkForm,
+    renderPlayers
   }
 })();
 
-const jerry = Player("Jerry", "X");
-const ben = Player("Ben", "O");
-displayController.renderBoard();
+const runGame = (() => {
+  let round = 0;
 
-gameBoard.boardDiv.addEventListener('click', (e) => displayController.makeChoice(e))
-gameBoard.formButton.addEventListener('click', (e) => displayController.checkForm())
+  const playerAssign = (playerOne, playerTwo) => {
+    const first = Player(playerOne, "X");
+    const second = Player(playerTwo, "O");
+    displayController.renderPlayers(first, second);
+  }
+
+  const updateArray = (square, sign) => {
+    const index = parseInt(square.slice(-1)) - 1;
+    gameBoard.gameArray[index] = sign;
+    checkWin(gameBoard.gameArray, sign);
+  }
+
+  const checkWin = (array, sign) => {
+    if (sign === "X") round++;
+
+    if (round < 3) return;
+
+    console.log('haha')
+  }
+
+  return {
+    playerAssign,
+    updateArray
+  }
+
+})()
